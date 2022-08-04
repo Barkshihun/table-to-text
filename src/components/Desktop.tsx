e73c8a89d23e5ea3cf0053ca6311ca9a58799f79
Barkshihun <barkshihun2@gmail.com>
2022-08-04 23:07:17 +0900

    모바일 데스크탑 구분하여 렌더

---
+++ src/components/Desktop.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Desktop() {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>데스크톱</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Desktop;
---
+++ src/components/Mobile.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Mobile() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>모바일</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default Mobile;
