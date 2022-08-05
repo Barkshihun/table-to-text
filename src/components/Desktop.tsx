dfe33f0a4d244e827a0f237bb5cb8428c70b332f
Barkshihun <barkshihun2@gmail.com>
2022-08-05 14:10:23 +0900

    표 행 추가, 열 추가 버튼 구현

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -6,11 +6,21 @@ import TableContents from "./TableContents";
 
 function Desktop() {
   const [rows, setRows] = useState(2);
-  const [cols, setCols] = useState(5);
-  const onPlus = () => {};
-  const onMinus = () => {};
+  const [cols, setCols] = useState(2);
+  const onRowPlus = () => setRows((currentRow) => ++currentRow);
+  const onRowMinus = () => setRows((currentRow) => --currentRow);
+  const onColPlus = () => setCols((currentCol) => ++currentCol);
+  const onColMinus = () => setCols((currentCol) => --currentCol);
   return (
     <main>
+      <div>
+        <button onClick={onRowPlus}>ROW ➕</button>
+        <button onClick={onRowMinus}>ROW ➖</button>
+      </div>
+      <div>
+        <button onClick={onColPlus}>COLUMN ➕</button>
+        <button onClick={onColMinus}>COLUMN ➖</button>
+      </div>
       <table>
         <TableContents rows={rows} cols={cols} />
       </table>
