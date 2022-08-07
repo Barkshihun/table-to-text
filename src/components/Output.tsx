792cac0f0b7ffe5955a8e65154e245881be6a356
Barkshihun <barkshihun2@gmail.com>
2022-08-07 15:03:41 +0900

    표를 text로 줄바꿈해서 출력

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,17 +1,11 @@
 import { useState, useEffect } from "react";
 import { golbalRows, globalCols } from "./Table";
 import { globalInputs } from "./TableContents";
-
+// col이 가로 row가 세로
 function Output() {
   const [text, setText] = useState("변환에 실패하였습니다");
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     // setText(event.target.value);
-  };
-  const globalInputsToText = (globalInputs: {
-    [inputName: string]: string;
-  }) => {
-    let temp: string[] = [];
-    return temp.toString();
   };
   // console.log("globalInputs", globalInputs);
   const getLongestTextPerCol = (globalInputs: tableInputObj) => {
@@ -25,8 +19,42 @@ function Output() {
     }
     return longestTextPerCol;
   };
+  const getTableList = (globalInputs: tableInputObj) => {
+    let tableList = new Array(golbalRows);
+    for (let i = 0; i < tableList.length; i++) {
+      tableList[i] = new Array(globalCols);
+    }
+    for (let coord in globalInputs) {
+      const row = parseInt(coord.split(",")[0]);
+      const col = parseInt(coord.split(",")[1]);
+      tableList[row][col] = globalInputs[coord];
+    }
+    return tableList;
+  };
+  const tableList = getTableList(globalInputs);
   const longestTextPerCol = getLongestTextPerCol(globalInputs);
-  console.log("longestTextPerCol", longestTextPerCol);
+  const globalInputsToText = (tableList: string[][]) => {
+    let tempList: string[] = [];
+    for (let i = 0; i < tableList.length; i++) {
+      for (let j = 0; j < tableList[i].length; j++) {
+        tempList.push(tableList[i][j]);
+      }
+      tempList.push("\n");
+    }
+    tempList.pop;
+    return tempList.join("");
+  };
+  useEffect(() => {
+    setText(globalInputsToText(tableList));
+  }, []);
+  console.log(
+    "longestTextPerCol",
+    longestTextPerCol,
+    "globalInputs",
+    globalInputs,
+    "globalInputsToText(globalInputs)",
+    globalInputsToText(tableList)
+  );
   return (
     <>
       <textarea cols={30} rows={10} value={text} onChange={onChange}></textarea>
@@ -34,3 +62,14 @@ function Output() {
   );
 }
 export default Output;
+/*
+    let temp = [];
+    for (let row = 0; row < tableList.length; row++) {
+      for (let col = 0; col < tableList[row].length; col++) {
+        temp.push(tableList[row][col]);
+      }
+      temp.push("\n");
+    }
+    temp.pop();
+    return temp.join("");
+*/
