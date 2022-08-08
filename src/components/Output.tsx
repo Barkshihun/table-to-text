e646c20dd32fa4715f1b31ff90ee201d4a2e7099
Barkshihun <barkshihun2@gmail.com>
2022-08-08 23:02:23 +0900

    테두리 없애기 전

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -7,48 +7,50 @@ const VERTICAL_CHAR = "|";
 const HORIZONTAL_BORDER_CHAR = "#"; // ■
 const VERTICAL_BORDER_CHAR = "#";
 
-const thickChars: string[] = ["@", "\u25A0-\u25FF"];
-function Output({
-  isVerticalLine,
-  isHorizontalLine,
-  isBorder,
-}: {
-  isVerticalLine: boolean;
-  isHorizontalLine: boolean;
-  isBorder: boolean;
-}) {
+const thickChars = ["@", "\u25A0-\u25FF"];
+const thinChars = ["-"];
+const bitThickChars = [""];
+function Output({ isVerticalLine, isHorizontalLine, isBorder }: { isVerticalLine: boolean; isHorizontalLine: boolean; isBorder: boolean }) {
   // 정규표현식 시작
-  // +2로 계산
+  // +20로 계산  +2
   const regCJK = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ぁ-ゔ|ァ-ヴー|々〆〤|一-龥]/g;
   const regThick = new RegExp(`[${thickChars.join("|")}]`, "g");
-  //1로 계산
+  // +5로 계산  +0.5 (1보다 작은 글자)
+  const regThin = new RegExp(`[${thinChars.join("|")}]`, "g");
+  // +15로 계산 +15
+  const regBitThick = new RegExp(`[${bitThickChars.join("|")}]`, "g");
+  // +10로 계산  +1
   const regEng = /[a-z|A-Z]/g;
-  const regElse = new RegExp(
-    `[^ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ぁ-ゔ|ァ-ヴー|々〆〤|一-龥|a-z|A-Z|${thickChars.join(
-      "|"
-    )}]`,
-    "g"
-  );
+  const regElse = new RegExp(`[^ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ぁ-ゔ|ァ-ヴー|々〆〤|一-龥|a-z|A-Z|${thickChars.join("|")}|${thinChars.join("|")}|${bitThickChars.join("|")}]`, "g");
   // 정규표현식 끝
 
   const computeLength = (str: string) => {
     let textLength = 0;
-    let cjkList = str.match(regCJK);
-    let thickList = str.match(regThick);
-    let engList = str.match(regEng);
-    let elseList = str.match(regElse);
+    const cjkList = str.match(regCJK);
+    const thickList = str.match(regThick);
+    const engList = str.match(regEng);
+    const thinList = str.match(regThin);
+    const bitThickList = str.match(regBitThick);
+    const elseList = str.match(regElse);
     if (cjkList) {
-      textLength = textLength + cjkList.length * 2;
+      textLength = textLength + cjkList.length * 20;
     }
     if (thickList) {
-      textLength = textLength + thickList.length * 2;
+      textLength = textLength + thickList.length * 20;
     }
     if (engList) {
-      textLength = textLength + engList.length;
+      textLength = textLength + engList.length * 10;
     }
     if (elseList) {
-      textLength = textLength + elseList.length;
+      textLength = textLength + elseList.length * 10;
     }
+    if (bitThickList) {
+      textLength = textLength + bitThickList.length * 15;
+    }
+    if (thinList) {
+      textLength = textLength + thinList.length * 5;
+    }
+    textLength = Math.round(textLength / 10);
     return textLength;
   };
   const getLongestTextPerCol = () => {
@@ -66,19 +68,22 @@ function Output({
   };
   const longestTextPerColList = getLongestTextPerCol();
   const computeWidth = (str: string): number => {
+    let tableWidth = 0;
     const isCJK = regCJK.test(str);
     const isThick = regThick.test(str);
-    const tableWidth =
-      longestTextPerColList.reduce(
-        (previousValue, currentValue) => previousValue + currentValue,
-        0
-      ) +
-      cols * 3;
-    if (isCJK || isThick) {
-      return Math.round(tableWidth / 2);
-    } else {
-      return tableWidth;
+    const isThin = regThin.test(str);
+    const isBitThick = regBitThick.test(str);
+    tableWidth = longestTextPerColList.reduce((previousValue, currentValue) => previousValue + currentValue, 0) + cols * 3;
+    if (isThin) {
+      tableWidth = Math.round(tableWidth / 0.5);
     }
+    if (isBitThick) {
+      tableWidth = Math.round(tableWidth / 1.5);
+    }
+    if (isCJK || isThick) {
+      tableWidth = tableWidth + Math.round(tableWidth / 2);
+    }
+    return tableWidth;
   };
   const computeText = (row: number, col: number): string => {
     let text = globalTableList[row][col];
@@ -93,60 +98,73 @@ function Output({
     }
     return text;
   };
-  // 12
+  //
   const horizontalLineWidth = computeWidth(HORIZONTAL_CHAR);
   computeWidth("");
   const borderLineWidth = computeWidth(HORIZONTAL_BORDER_CHAR);
+  //
+  // console.log("horizontalLineWidth", horizontalLineWidth, "borderLineWidth", borderLineWidth);
+  const endOfRowIndex = rows - 1;
+  const endOfColIndex = cols - 1;
   const globalTableListToText = () => {
     let textList: string[] = [];
     if (isBorder) {
-      textList.push(`${HORIZONTAL_BORDER_CHAR.repeat(borderLineWidth + 1)}\n`);
+      textList.push(`${HORIZONTAL_BORDER_CHAR.repeat(borderLineWidth)}\n`);
     }
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
         let text = computeText(row, col);
-        if (isBorder && col === 0) {
-          text = `${VERTICAL_BORDER_CHAR} ${text}`;
-        }
-        if (col === cols - 1) {
+        if (col === 0) {
+          // col의 시작인가?
           if (isBorder) {
-            textList.push(`${text} ${VERTICAL_BORDER_CHAR}`);
-            continue;
+            text = `${VERTICAL_BORDER_CHAR}${" ".repeat(2)}${text}`;
+          } else {
+            text = `${" ".repeat(2)}${text}`;
           }
-          textList.push(`${text}`);
+        }
+        if (col === endOfColIndex && isBorder) {
+          // col의 끝이며 테두리를 그려야 하는가?
+          textList.push(`${text}${" ".repeat(2)}${VERTICAL_BORDER_CHAR}`);
           continue;
         }
         if (isVerticalLine) {
-          textList.push(`${text} ${VERTICAL_CHAR} `);
+          // 세로선이 있는가?
+          if (col === endOfColIndex) {
+            // col의 끝인가?
+            textList.push(`${text}`);
+            continue;
+          }
+          textList.push(`${text}${" ".repeat(2)}${VERTICAL_CHAR}${" ".repeat(2)}`);
           continue;
         }
-        textList.push(`${text}   `);
+        textList.push(`${text}${" ".repeat(2)}`);
       }
-      if (row === rows - 1) {
+      if (row === endOfRowIndex) {
+        // row의 마지막에서
         if (isBorder) {
-          textList.push(
-            `\n${HORIZONTAL_BORDER_CHAR.repeat(borderLineWidth + 1)}`
-          );
+          // 테두리가 있다면
+          textList.push(`\n${HORIZONTAL_BORDER_CHAR.repeat(borderLineWidth)}`);
           continue;
         }
         if (isHorizontalLine) {
+          // 가로선이 있다면 그리지 말고 넘겨라
           continue;
         }
       }
       if (isHorizontalLine) {
+        // 가로선을 그려야 하는가?
+        let horizontalLine;
         if (isBorder) {
-          textList.push(
-            `\n${VERTICAL_BORDER_CHAR}${HORIZONTAL_CHAR.repeat(
-              horizontalLineWidth - 1
-            )}${VERTICAL_BORDER_CHAR}\n`
-          );
-          continue;
+          horizontalLine = `\n${VERTICAL_BORDER_CHAR}${HORIZONTAL_CHAR.repeat(horizontalLineWidth)}${VERTICAL_BORDER_CHAR}\n`;
+        } else {
+          horizontalLine = `\n${HORIZONTAL_CHAR.repeat(horizontalLineWidth)}\n`;
         }
-        textList.push(`\n${HORIZONTAL_CHAR.repeat(horizontalLineWidth - 2)}\n`);
+        textList.push(horizontalLine);
         continue;
       }
       textList.push("\n");
     }
+    // console.log(textList);
     return textList.join("");
   };
   const [text, setText] = useState(globalTableListToText());
