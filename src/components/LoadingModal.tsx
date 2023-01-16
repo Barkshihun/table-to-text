dfc41b35765b9c765df9fb0a03e4f49bfd610ac6
Barkshihun <barkshihun2@gmail.com>
2023-01-17 02:13:32 +0900

    🎨 Format: LoadingModal 클래스명 수정

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -3,7 +3,7 @@ import "../scss/LoadingModal.scss";
 function LoadingModal() {
   return (
     <div className="loadingModal">
-      <div>
+      <div className="loadingModal__content">
         <h1>변환 중...</h1>
       </div>
     </div>
